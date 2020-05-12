1f9b949a35f64f39d106f8e3b4c6c8766c413193
thugo <thugomail@gmail.com>
2020-05-12 15:48:09 +0300

    Update hugi.py

--- hugi.py
+++ hugi.py
@@ -1,1 +1,11 @@
-#niggy
+from urllib.request import urlopen
+edetabel = urlopen("https://ratings.fide.com/top.phtml?list=men")
+baidid = edetabel.read()
+tekst = baidid.decode()
+eesnimi = str(input("Sisestage malemängja eesnimi: ")).lower()
+perenimi = str(input("Sisestage malemängja perekonnanimi: ")).lower()
+otsitav = perenimi.title() + ", " + eesnimi.title()
+algus = tekst.index(otsitav)
+temp_algus = algus + 53 + len(otsitav)  
+elo = tekst[temp_algus:temp_algus+4]       
+print(temp)
