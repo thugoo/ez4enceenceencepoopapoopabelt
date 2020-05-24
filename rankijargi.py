86423464fcae547343f93e43d22a5816c5c88dcf
thugo <thugomail@gmail.com>
2020-05-24 14:43:21 +0300

    Create rankijargi.py

---
+++ rankijargi.py
@@ -0,0 +1,38 @@
+from urllib.request import urlopen
+
+edetabel = urlopen("https://ratings.fide.com/top.phtml?list=men")
+baidid = edetabel.read()
+tekst = baidid.decode()
+
+asukoht = str(input("Sisestage malemängja asukoht edetabelis: ")).lower()
+
+otsitav = "<td width=10>&nbsp;" + asukoht + "</a></td>"
+algus = tekst.index(otsitav)
+
+sisaldabnime = tekst[algus:algus+200]
+nimi = sisaldabnime[sisaldabnime.index("class=tur")+len("class=tur")+1:sisaldabnime.index("</a></td><td>&nbsp;g")]
+
+info = tekst.index(nimi) + 53 + len(nimi)  
+elo = tekst[info:info+4]
+
+i = 9266
+asukoht = 0
+
+while i < algus:
+    i += 225
+    asukoht += 1
+    
+nimi = nimi.replace(",", "")
+nimejärjend = nimi.split()
+täisnimi = ""
+for nimi in reversed(nimejärjend):
+    if nimejärjend.index(nimi) == 0:
+        täisnimi += nimi
+    else:
+        täisnimi += nimi + " "
+
+infojärjend = ['Mängija nimi (eesnimi- ja perekonnanimi): ' + täisnimi]
+infojärjend.append('Elo reiting: ' + elo)
+infojärjend.append('Asukoht FIDE top 100 mängijate edetabelis: ' + str(asukoht) + '. kohal')
+for i in infojärjend:
+    print(i)
